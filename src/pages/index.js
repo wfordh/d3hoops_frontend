78ffea416e2e604e6b0e4e4061849f1241ad958d
Ford Higgins <wfordh@users.noreply.github.com>
2021-05-04 12:59:02 -0700

    some text updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to my new Gatsby site. It will be the future home of D3 men's basketball stats.</p>
+    <h1>No TV Timeouts</h1>
+    <p>Welcome to my site for D3 men's basketball statistics'. I created it to fill in the stats gap for Division III hoops and was inspired by <a href="https://www.kenpom.com">KenPom</a>, <a href="https://www.barttorvik.com">barttorvik</a>, and <a href="https://www.herhoopstats.com">Her Hoop Stats</a>.</p>
     <p>Now go build something great.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
@@ -20,8 +20,8 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <Link to="/about/">Go to my about page.</Link> <br />
+      <Link to="/using-advanced-stats/">Go to "Using advanced stats"</Link>
     </p>
   </Layout>
 )
