4d418eda6505e4fc4585d68eb38b26ce65a96334
Ford Higgins <wfordh@users.noreply.github.com>
2021-06-12 11:34:43 -0700

    making team page

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,8 +11,25 @@ exports.createPages = async ({ graphql, actions: { createPage } }) => {
   const result = await graphql(`
     {
       postgres {
+        team_season_stats: allTeamSeasons {
+          team_id,
+          season,
+          num_games,
+          fg_pct,
+          fg3_pct,
+          ft_pct
         }
       }
     }
-  `)
+  `);
+  
+  result.data.postgres.team_season_stats.forEach((team_season) => {
+    createPage({
+      path: slugifyPost(team_season),
+      component: path.resolve(".src/templates/team-page.js"), 
+      context: {
+        teamSeason: `{team_season.team_id}_{team_season.season}`
+      },
+    });
+  });
 }
--- src/templates/team-page.js
+++ src/templates/team-page.js
@@ -1,7 +1,26 @@
 // template for each team's page
+// reference: https://github.com/graphile/gatsby-source-pg-example/blob/master/src/templates/blog-post.js
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby";
 
-export default function teamPage() {
-  return ()
-}
+const teamPage = ({ data }) => {
+  const { teamStats } = data.postgres;
+  // add table
+  return (
+    <Layout>
+      <div>
+        <h1>{teamStats.id} - {teamStats.season}</h1>
+        <table>
+        </table>
+      </div>
+    </Layout>
+  );
+};
+
+export default teamPage;
+
+export const query = graphql`
+  query () {
+  }
+`;
